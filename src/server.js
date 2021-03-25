ff0a852fc02f67cf8a64dfad145ec4329204994b
João Gabriel Ramos <joaogabrielramos28@outlook.com>
2021-03-24 21:36:37 -0300

    🚧  Começando a fazer as views e rotas do projeto

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars')
 const bodyParser = require("body-parser")
-//Configurando body-parser
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+//Config
+    //Body-Parser
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({extended:false}))
+    //Template Engine
+        app.engine('handlebars',handlebars({defaultLayout:'main'}))
+        app.set('view engine','handlebars')
+
+
+
+//Rotas
+    //Get
+        app.get("/",(req,res)=>{
+            res.render('home')
+        })
 
 
 //Inicializando o servidor na porta 3000
