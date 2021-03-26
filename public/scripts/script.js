7c2a5d2223849c4a81f1876cbf590794d7cce753
João Gabriel Ramos <joaogabrielramos28@outlook.com>
2021-03-25 22:31:08 -0300

    🚧  Pegando estados e municipios da API do IBGE

---
+++ public/scripts/script.js
@@ -0,0 +1,48 @@
+//*! Pegando dados da api do governo 
+// * Pegando os estados
+const select = document.querySelector("select")
+function pegaEstados(){
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((res)=>{
+        res.json()
+        .then((data)=>{
+            for (i = 0 ; i < data.length;i++){
+            const option = document.createElement('option')
+            option.textContent = data[i].sigla
+            option.setAttribute("value",data[i].id)
+            select.appendChild(option)
+
+            }
+            console.log(data);
+        })
+
+    }).catch(erro =>{
+        console.log("Fetch Failed",erro);
+    })
+}
+
+pegaEstados()
+const selectMunicipio = document.querySelector("#a")
+// * Pegando os municipios de acordo com a mudança de evento dos estados
+select.addEventListener("change",()=>{
+    function pegaCidades(estado){
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/distritos`)
+        .then((res)=>{
+            res.json()
+            .then((data)=>{
+                for(i=0;i<data.length;i++){
+                const options = document.createElement("option")
+                options.textContent  = data[i].nome
+                selectMunicipio.appendChild(options)
+                }
+            })
+        })
+    }
+    pegaCidades(select.value)
+    selectMunicipio.length = 0
+})
+
+
+
+
+
